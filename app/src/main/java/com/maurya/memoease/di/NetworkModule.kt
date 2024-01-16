1569ac06077ed3f133d0349ec58e06bfeaffb2ea
notrealmaurya <loop.maurya@gmail.com>
2024-01-16 13:00:50 +0530

    MemoEase

--- app/src/main/java/com/maurya/memoease/di/NetworkModule.kt
+++ app/src/main/java/com/maurya/memoease/di/NetworkModule.kt
@@ -1,4 +1,35 @@
 package com.maurya.memoease.di
 
+import com.maurya.memoease.Constants.BASE_URL
+import com.maurya.memoease.api.UserAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
 class NetworkModule {
+
+
+    @Singleton
+    @Provides
+    fun providesRetrofit():Retrofit{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesUserAPI(retrofit: Retrofit):UserAPI{
+        return  retrofit.create(UserAPI::class.java)
+    }
+
+
+
 }
