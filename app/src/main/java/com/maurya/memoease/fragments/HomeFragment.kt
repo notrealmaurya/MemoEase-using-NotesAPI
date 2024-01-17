5148e8cad11551b5338bb28bd33e84d29012c118
notrealmaurya <loop.maurya@gmail.com>
2024-01-17 22:15:23 +0530

    Added NoteFragment

--- app/src/main/java/com/maurya/memoease/fragments/HomeFragment.kt
+++ app/src/main/java/com/maurya/memoease/fragments/HomeFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
@@ -16,8 +15,7 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import com.google.gson.Gson
 import com.maurya.memoease.AdapterNotes
-import com.maurya.memoease.ApplicationMemoEase
-import com.maurya.memoease.NoteViewModel
+import com.maurya.memoease.models.NoteViewModel
 import com.maurya.memoease.R
 import com.maurya.memoease.databinding.FragmentHomeBinding
 import com.maurya.memoease.models.NoteResponse
